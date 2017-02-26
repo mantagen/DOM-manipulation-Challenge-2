46e1401deea6e4a521dd106eaf8dabdeee37d63a
mantagen <mathsgregory@gmail.com>
2017-02-26 18:18:38 +0200

    Removing solutions from js

--- exercise/main.js
+++ exercise/main.js
@@ -2,41 +2,24 @@ var formList = {
   container: document.querySelector("#form-list-container"), // query dom for #form-list-container
   getValuesArray: function(){
     // write a function which returns input values as an array
-    return Array.prototype.slice
-      .call(formList.container.querySelectorAll("input"))
-      .map(function(input){
-        return input.value;
-      })
-      .filter(function(value){
-        return value;
-      });
+
   },
   sortByAlpha: function(array){
     // return a sorted copy of the array
-    return array.concat().sort();
+
   },
   generateUl: function(array){
     // write a function which returns a ul html element containing li's for
     // each array element
-    var ulNode = document.createElement("ul");
-    var liNode;
-    var textNode;
-    array.forEach(function(elem){
-      if(typeof elem === "string" || typeof elem === "integer");
-      liNode = document.createElement("li");
-      textNode = document.createTextNode(elem);
-      liNode.appendChild(textNode);
-      ulNode.appendChild(liNode);
-    });
-    return ulNode;
+
   },
   removeUl: function(){
     // write a function which removes the ul from the page
-    var ul = formList.container.querySelector("ul");
-    if(ul){
-      formList.container.removeChild(ul);
-    }
+
   },
+  addUl: function(ul){
+    // write a function which adds the
+  }
   checkElements: function(){
     if(!formList.container){
       return console.error("No form container found");
@@ -52,7 +35,9 @@ var formList = {
     return true;
   },
   init: function(){
-    formList.checkElements();
+    if(!formList.checkElements()){
+      return console.error("App could not start");
+    };
     // add a click listener to the save button, which adds the input values
     // as a ul on the page
     formList.saveButton.addEventListener("click", function(){
